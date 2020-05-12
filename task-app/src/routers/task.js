b74fe6854f6e142e05c39883a5f6b19fad5c5423
abdallahragab40 <abdallahragab40@gmail.com>
2020-05-12 23:28:50 +0200

    add filter , sort and pagination to tasks

--- task-app/src/routers/task.js
+++ task-app/src/routers/task.js
@@ -20,11 +20,36 @@ router.post("/tasks", auth, async (req, res) => {
 });
 
 // READ
+// GET /tasks?completed=true
+// GET /tasks?limit=2&skip=2
+// GET /tasks?sortBy=createdAt:asc
 router.get("/tasks", auth, async (req, res) => {
   try {
     //const tasks = await Task.find({});
     //const tasks = await Task.find({ owner: req.user._id });
-    await req.user.populate("tasks").execPopulate();
+    const match = {};
+    const sort = {};
+
+    if (req.query.completed) {
+      match.completed = req.query.completed === "true";
+    }
+
+    if (req.query.sortBy) {
+      const parts = req.query.sortBy.split(":");
+      sort[parts[0]] = parts[1] === "desc" ? -1 : 1;
+    }
+
+    await req.user
+      .populate({
+        path: "tasks",
+        match,
+        options: {
+          limit: parseInt(req.query.limit),
+          skip: parseInt(req.query.skip),
+          sort,
+        },
+      })
+      .execPopulate();
     res.send(tasks);
   } catch (e) {
     res.status(500).send(e);
